c61f68b2e55aff22aadb4e10e4da520806054db0
Ashwini Visave <ashwini.visave@nepa.com>
2025-04-01 21:23:07 +0200

    Add logging

--- twitter-reader-service/read-messages-from-x.py
+++ twitter-reader-service/read-messages-from-x.py
@@ -1,9 +1,11 @@
 import tweepy
 import os
 import requests
+import logging
 
 
 # Set up Twitter API credentials
+logging.info("Setting up Twitter API credentials...")
 API_KEY = os.getenv('TWITTER_API_KEY')
 API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY')
 ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
@@ -14,6 +16,7 @@ BEARER_TOKEN = os.getenv('TWITTER_BEARER_TOKEN')
 api_url = "http://api_service:5000/predict"
 
 # Authenticate to Twitter API
+logging.info("Authenticating to Twitter API...")
 client = tweepy.Client(bearer_token=BEARER_TOKEN)
 
 def search_tweets(query, max_results=100):
@@ -22,6 +25,7 @@ def search_tweets(query, max_results=100):
         # requests.post(api_url, json=response["Review"])
         
         # Search for tweets matching the query
+        logging.info(f"Searching for tweets with query: {query}")
         response = client.search_recent_tweets(
             query=query,
             max_results=max_results,
@@ -36,14 +40,15 @@ def search_tweets(query, max_results=100):
                         "lang": tweet.lang
                     }
                     # Send the tweet to the REST API for prediction
+                    logging.info("Sending tweet to API...")
                     response = requests.post(api_url, json=tweet["Review"])
-                    print("Response from API:", response.json())
+                    logging.info("Response from API:", response.json())
         else:
             return "No tweets found."
 
     except tweepy.TooManyRequests as e:
-        print(f"Error: {e}")
-        print("Rate limit exceeded. Wait for reset.")
+        logging.info(f"Error: {e}")
+        logging.info("Rate limit exceeded. Wait for reset.")
 
 # tweets_data = search_tweets('US Elections')
 # print(tweets_data)
